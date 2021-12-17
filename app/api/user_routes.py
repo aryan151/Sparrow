1c8a50c8a6c139a2e3a2af32f21d5eeb02383316
aryan <aryanattula@gmail.com>
2021-12-17 17:54:14 -0500

    all user information backend routes

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request 
 from flask_login import login_required
-from app.models import User, db, Watchlist
+from app.models import User, db, Watchlist, Asset
 
 user_routes = Blueprint('users', __name__)
 
@@ -25,3 +25,24 @@ def update_buying_power(id):
     user.buying_power = body
     db.session.commit()
     return 'ok'
+
+@user_routes.route('/<int:id>/assets/')
+def get_all_assets(id):
+    assets_from_db = Asset.query.filter(Asset.user_id == int(id)).all()  
+    assets = { asset.to_dict()['ticker']: asset.to_dict() for asset in assets_from_db }
+    return assets
+
+
+@user_routes.route('/<int:id>/watchlists/', methods=['GET', 'POST'])
+def watchlists(id):
+    body = request.json
+    if request.method == 'GET':  
+        Watchlists = Watchlist.query.filter(Watchlist.user_id == id).all()
+        return {Watchlist.to_dict()['id']: Watchlist.to_dict() for Watchlist in Watchlists}
+    elif request.method == 'POST':
+        body = request.json
+        new_watchlist = Watchlist(user_id=id, list_name=body["Watchlist_name"])
+        db.session.add(new_watchlist)
+        db.session.commit()
+        return new_watchlist.to_dict()    
+
