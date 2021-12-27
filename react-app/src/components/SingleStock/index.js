586a730c1b0b477ccf3beea63f4846fae50e089e
aryan <aryanattula@gmail.com>
2021-12-27 17:39:37 -0500

    All Stock Information Working

--- react-app/src/components/SingleStock/index.js
+++ react-app/src/components/SingleStock/index.js
@@ -1,9 +1,23 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useState, useEffect } from "react";      
 import { useParams } from 'react-router-dom'
+import {setCurrentStock} from '../../store/currentStock'
+
+
 
 function SingleStock () {
+    const dispatch = useDispatch()  
     const {ticker} = useParams() 
+
+    const [resolution, setResolution] = useState('D');
+
+    useEffect(()=>{
+        (async ()=>{
+          await dispatch(setCurrentStock(ticker, resolution))
+        })()
+      },[resolution, ticker])  
+
+
     return (
         <div>
         <p> single stock</p>   
