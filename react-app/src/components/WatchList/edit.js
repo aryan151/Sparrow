f408cab8b3966e4f93183e57143984d0a5cea1f7
aryan <aryanattula@gmail.com>
2021-12-29 21:07:32 -0500

    Added Edit Watchlist

--- react-app/src/components/WatchList/edit.js
+++ react-app/src/components/WatchList/edit.js
@@ -7,4 +7,19 @@ import Main from "../SingleStock/StockChart/main";
 
 function Edit({list, setShowModal}) {  
     const dispatch = useDispatch()   
+    const graphData = useSelector(state => state.watchlistStocks)
+    const userAssetsGraph = useSelector(state => state.userAssets.graphData)
+
+    const overAllIsPos = userAssetsGraph?.[userAssetsGraph.length - 1]['%'][0] === '+' ? 'pos' : 'neg'
+
+    const [newListTitle, setNewListTitle] = useState(list.watchlistName)  
+    let symbols = Object.keys(list.tickers);  
+
+    return (
+        <div>
+            <p>TEXT HERE</p>
+        </div>
+    )
 }
+
+export default Edit 
--- react-app/src/components/WatchList/item.js
+++ react-app/src/components/WatchList/item.js
@@ -2,13 +2,17 @@ import { useDispatch } from "react-redux";
 import { useEffect, useState } from "react"; 
 import { useSelector } from "react-redux";
 import { setWatchListStocks } from "../../store/watchlistStocks"; 
-import {IoIosArrowDown, IoIosArrowUp} from 'react-icons/io'   
+import {IoIosArrowDown, IoIosArrowUp} from 'react-icons/io'  
+import Edit from "./edit"; 
+import {Modal} from '../../context/Modal'  
 import {BiDotsHorizontal} from 'react-icons/bi'     
 import ItemData from "./itemchart";
 
 function Item ({list, listName=false , isStocks=false, isPos}){ 
     const dispatch = useDispatch();    
     const [showList, setShowList] = useState(true)
+    const [showModal, setShowModal] = useState(false)
+
     const refListLength = Object.keys(list?.tickers).length  
     
     let tickers = Object.keys(list?.tickers);  
@@ -25,11 +29,17 @@ function Item ({list, listName=false , isStocks=false, isPos}){
     return (
       <>
         <div className={`${listName ? 'stock-title' : `list-title-edit`}`}>
-          <h1>{listName ? listName : list.listName}</h1>
+          <h1>{listName ? listName : list.watchlistName}</h1>   
           <div className="list-settings">
+              {!isStocks && <BiDotsHorizontal onClick={() => setShowModal(!showModal)} className={`${isPos}-menu`}/>}
               {showList ? <IoIosArrowUp onClick={() => refListLength ? setShowList(!showList) : null} id="up" className={`${isPos}-arrow`} /> : <IoIosArrowDown onClick={() => refListLength ? setShowList(!showList) : null} id="down" className={`${isPos}-arrow`}/>}
-          </div>
+          </div>  
         </div>  
+        {showModal && (
+          <Modal onClose={() => setShowModal(false)} isWatchList={true}>
+            <Edit setShowModal={setShowModal} list={list} />  
+          </Modal>
+        )}
         {showList && (
           <>
                    
